167481d3d18b8d4d295416a0ad0145e462fde228
Artur Lesniewski <arthur_las@yahoo.co.uk>
2018-07-20 17:31:39 +0100

    first commit

---
+++ src/search.js
@@ -0,0 +1,72 @@
+import React, {
+    Component
+} from 'react'
+import './App.css'
+
+class Search extends Component {
+
+
+
+    state = {
+        book: [],
+        query: '',
+        addshelf: {
+            shelf: ''
+        }
+    }
+
+
+
+    handleChange(bookid, event) {
+        console.log(this.props.books[0])
+
+
+        var shelf = event.target.value;
+        var id = this.props.books.map((book) => {
+            return (book.id === bookid ? book.shelf = shelf : book.shelf = book.shelf)
+        })
+
+
+
+        let kniga = this.props.books.filter((book) => book.id === bookid)
+        console.log(kniga)
+        this.props.test(kniga, shelf);
+
+    }
+
+
+
+    render() {
+
+
+        return (
+
+
+            <div     style={{display: 'flex', flexFlow: 'row wrap', justifyContent: 'center'  }}>
+ 
+       {this.props.books.map((book) => 
+     <li style={{padding:'10px 15px', listStyleType: 'none'}}><div     div className="book"  key={book.id}>
+                          <div className="book-top">
+                            <div className="book-cover" key={book.id} style={{ width: 128, height: 174, backgroundImage:  book.imageLinks ? 'url('+book.imageLinks.thumbnail+')' :''}}></div>
+                            <div className="book-shelf-changer">
+                              <select id="mySelect"  onChange={  this.handleChange.bind(this,book.id)} >
+                                <option value="move" disabled>Move to...</option>
+                                <option value='' style={{display:'none'}}> </option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title" > {book.title }</div>
+                          <div className="book-authors">{ book.authors ? book.authors.toString() : ''  }</div>
+                        </div></li>
+                )}   
+
+        </div>
+
+        )
+    }
+}
+export default Search
---
+++ src/test.js
@@ -0,0 +1,64 @@
+import React, {
+    Component
+} from 'react'
+import './App.css'
+
+class Test extends Component {
+
+
+    state = {
+        shelf: 'currentlyReading'
+    }
+
+
+
+    handleChange(bookid, event) {
+        console.log(bookid)
+        var shelf = 'currentlyReading'
+        shelf = event.target.value;
+        var id = this.props.books.map((book) => {
+            return (book.id === bookid ? book.shelf = shelf : book.shelf = book.shelf)
+        })
+
+
+        let kniga = this.props.books.filter((book) => book.id === bookid)
+        this.props.test(kniga, id);
+
+
+    }
+
+
+    render() {
+        return (
+
+            <div  style={{display: 'flex', flexFlow: 'row wrap', justifyContent: 'center'}}>
+              
+
+       {this.props.books.map((book)=>  { return (( book.shelf==='currentlyReading')  
+
+    ?   <li  ><div className="book" key={book.id} >
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:  book.imageLinks ? 'url('+book.imageLinks.thumbnail+')' :'' }}></div>
+                            <div className="book-shelf-changer">
+                              <select id="mySelect"  onChange={   this.handleChange.bind(this,book.id)}  >
+                                <option value="move" disabled>Move to...</option>
+                                <option value='' style={{display:'none'}}> </option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title" > {book.title }</div>
+                          <div className="book-authors">{ book.authors ? book.authors.toString() : ''  }</div>
+                        </div></li > : null
+                ) } )} 
+
+
+       
+ </div>
+        )
+    }
+}
+export default Test
